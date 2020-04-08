4b0744fdbd02a89ea7505ea7560ba95255645299
Hervé <herve.neuenschwander@etu.hesge.ch>
2020-04-08 14:11:20 +0200

    refactor du component et du screen login

--- components/login-form.js
+++ components/login-form.js
@@ -7,10 +7,6 @@ import {
   heightPercentageToDP as hp
 } from 'react-native-responsive-screen';
 
-import {dbo} from '../dataObjects/dbo';
-
-const firebase = require('firebase');
-
 class LoginForm extends Component{
   constructor(props){
     super(props)
@@ -28,11 +24,7 @@ class LoginForm extends Component{
   }
   handleLogin=()=>{
     if(this.state.password !== "" && this.state.email !== ""){
-      dbo.handleLogin(this.state.email,this.state.password)
-        .then(user=>{
-          this.props.connectUser(user)
-          this.props.navigation.replace('Home');
-        })
+      this.props.handleLogin(this.state.email,this.state.password);
     }
   }
   render(){
